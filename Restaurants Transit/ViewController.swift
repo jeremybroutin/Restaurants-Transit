402f21d42e267db19e9a441b1b7a9628430799d3
Jeremy Broutin <jeremybroutin@jeremybroutin-macbookpro3.roam.corp.google.com>
2017-05-06 09:28:17 -0700

    Initial Commit

---
+++ Restaurants Transit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurants Transit
+//
+//  Created by Jeremy Broutin on 5/6/17.
+//  Copyright © 2017 Jeremy Broutin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
